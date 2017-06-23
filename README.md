0b512e688ab1f80879d4d844decef8aec61975a7
ogalex <alexburtson@Alexs-MacBook-Pro.local>
2017-06-23 12:43:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,172 @@
+#SlideSelector
+
+This plugin includes a module with multiple selectable slides.
+
+This is a multi-purpose module that can be used whenever you desire a responsive layout that shows one slide at a time. When clicking a selector, the current slide content will fade out before your clicked selector's corresponding content fades in.
+
+##Initial setup
+
+1. _Dependencies:_ verify that the jQuery is included in your project.
+2. Include **slide-selector.js** in your JS directory.
+3. Include **_slide-selector.scss** or **slide-selector.css** in your stylesheets.
+4. Include the HTML inside **slide-selector.html** somewhere on your site.
+5. Run gulp, and your slideSelector will be up and running.
+
+##Working with HTML
+
+###Adding new slides
+
+If you want to add another slide to your slideSelector, insert another `<li>` inside `.ss__selectors` to add a new label, as well as another `<div>` inside `.ss__slides` to add corresponding slide content, like so:
+
+```html
+<div class="ss">
+	<ul class="ss__selectors">
+		<li>...</li>
+		<li>...</li>
+		<li>...</li>
+		<li></li> <!-- your new li -->
+	</ul>
+	<div class="ss__slides">
+		<div>...</div>
+		<div>...</div>
+		<div>...</div>
+		<div></div> <!-- your new div -->
+	</div>
+</div>
+```
+
+Make sure to insert both elements in the same spot relative to each other (ie, last, first, second, etc).
+
+Feel free to remove slides if need be. The HTML includes three slides by default, but two works fine as well.
+
+##Working with JS
+
+###The slideSelector function
+
+At the bottom of the __slide-selector.js__ file, you will find this function:
+
+```javascript
+$('.ss').slideSelector();
+```
+
+This function creates your slideSelector.
+
+If you want to customize the display settings of your slideSelector, include any of the below options, like so:
+
+```javascript
+$('.ss').slideSelector({
+    initialSlide: 2,
+    speed: 200,
+    breakpoint: 900,
+    mobileStyle: 'toggle',
+    mobileOnly: false,
+    togglesOpen: true
+});
+```
+
+#####initialSlide
+
+Sets which slide is active, `1` being the first. Default is `1`.
+
+#####speed
+
+Sets the fade transition time (in milliseconds). Default is `300`.
+
+#####breakpoint
+
+Determines the mobile breakpoint. If screen width is less than or equal to this number (in pixels), the mobile layout will be shown. Default is `768`.
+
+#####mobileStyle
+
+Provides alternative mobile layouts.
+
+* `'list'`: displays all slide content vertically, paired with labels.
+* `'toggle'`: displays all slide content vertically, paired with labels which can be clicked to show/hide the content. Content is hidden by default.
+
+If mobileStyle is not defined, or the above options are not selected, your slideSelector will show the desktop layout on all screen sizes.
+
+#####mobileOnly
+
+If `true` and if `mobileStyle` is defined, the chosen mobile layout will replace the desktop layout at every screen size. Default is `false`.
+
+#####togglesOpen
+
+If `true` and if `mobileStyle` is set to `'toggle'`, content for all slides will be visible on mobile layouts.
+
+###Creating multiple versions
+
+slideSelector only works on the provided HTML base structure, which has the class `.ss`. If you want to target all slideSelectors on your site with the same settings, keep the function like so:
+
+ ```javascript
+ $('.ss').slideSelector();
+ ```
+ 
+Otherwise, you can add classes or ids to different instances of the HTML, and run another function, like so:
+ 
+ ```javascript
+ $('.your-class').slideSelector();
+ $('.your-other-class').slideSelector();
+ ```
+ 
+You can keep these functions inside the __slide-selector.js__ file, or add it to another JS file, as long as it is included on your site after __slide-selector.js__.
+
+####Unique settings for each version
+
+You can even add different settings in each function for further customization, like so:
+
+```javascript
+$('.your-class').slideSelector({
+    initialSlide: 3,
+    speed: 400,
+    breakpoint: 480,
+    mobileStyle: 'list'
+});
+
+$('.your-other-class').slideSelector({
+    speed: 200,
+    mobileStyle: 'toggle',
+    mobileOnly: true
+});
+```
+
+__Note:__ If you do create different versions of the slideSelector, make sure to avoid including the default `$('.ss').slideSelector();` on the same HTML page as your other slideSelector versions. 
+
+##Working with CSS
+
+
+###Mobile modifiers
+There are a few important modifier classes to know about when adding your own styles:
+
+* `.ss--mobile`: added to the slideSelector when the mobile layout is visible. Check your `breakpoint` settings in your JS to know when your mobile layout will be visible.
+* `.ss--mobile--list`: added to the slideSelector if `mobileStyle` is set to `'list'`.
+* `.ss--mobile--toggle`: added to the slideSelector if `mobileStyle` is set to `'toggle'`.
+* `.ss--mobile--default`: added to the slideSelector if `mobileStyle` is undefined. Out of the box, no styles are assigned to this class (so there will be no visible mobile layout changes), although `.ss--mobile` will still be added to the slideSelector based on your `breakpoint` setting, so feel free to take advantage of this.
+
+###Additional elements
+
+The JS adds additional elements to the HTML in order to provide alternative layouts for mobile. Below is an explanation of where to find these elements, and what they do.
+
+If you inspect a slide in your browser using developer tools, you will find that the layout is different than in your original HTML. It will look something like this:
+
+```html
+<div class="ss">
+	<ul class="ss__selectors">...</ul>
+	<div class="ss__slides">
+		<div>
+			<div class="ss__label">...</div>
+			<div class="ss__content">
+				<div class="ss__content__inner">...</div>
+			</div>
+		</div>
+		...
+	</div>
+</div>
+```
+
+Inside `.ss__slides > div`, there are the following elements:
+
+* `.ss__label`: contains a copy of the HTML inside the corresponding `.ss__selectors > li`. This serves as the label and toggle for the slide content on mobile layouts, and is hidden on the desktop layout.
+* `.ss__content`: wrapper for the slide content. This is needed to set the height of the content when hiding/showing the slide content on mobile layouts.
+* `.ss__content__inner`: exists inside `.ss__content`. This contains the HTML slide content initially placed inside the parent container, `.ss_slides > div`. If `mobileStyle` is set to `'toggle'`, `.ss__content__inner` will animate opacity values to fade in/out the slide content on mobile layouts.
+
+These elements are accessible on the page at every screen size. Feel free to add your own styles, and use the mobile modifier classes explained above for more fine tuning.
---
+++ slide-selector.js
@@ -0,0 +1,299 @@
+$.fn.slideSelector = function(settings) {
+    new SlideSelector(this, settings);
+};
+
+function SlideSelector(element, settings){
+    if (settings === undefined) { 
+        settings = {}; 
+    }
+    
+    // default settings
+    this.module = $(element);
+    this.initialSlide = 1;
+    this.speed = 300;
+    this.breakpoint = 768;
+    this.mobileStyle = undefined;
+    this.mobileOnly = false;
+    this.togglesOpen = false;
+
+    // custom settings
+    if ($(element).hasClass('ss') == false) {
+        this.module = $('.ss');
+    }
+    if (settings.initialSlide) {
+        this.initialSlide = settings.initialSlide;
+    }
+    switch (settings.speed) {
+        case 0:
+            this.speed = 0;
+            break;
+        case false:
+            break;
+        default:
+            this.speed = settings.speed;
+            break;
+    }
+    switch (settings.breakpoint) {
+        case 0:
+            this.breakpoint = 0;
+            break;
+        default:
+            this.breakpoint = settings.breakpoint;
+            break;
+    }
+    if (settings.mobileStyle) {
+        this.mobileStyle = settings.mobileStyle;
+    }
+    if (settings.mobileOnly) {
+        this.breakpoint = 999999;
+    }
+    if (settings.togglesOpen) {
+        this.togglesOpen = settings.togglesOpen;
+    }
+
+    // elements
+    this.selectorsWrapper = this.module.children('.ss__selectors');
+    this.selectors = this.selectorsWrapper.children('li');
+    this.slidesWrapper = this.module.children('.ss__slides');
+    this.slides = this.slidesWrapper.children('div');
+
+    // functions
+    this.setMobileStyle();
+    this.setActiveSlide();
+    this.setSlideHeight();
+    this.changeActiveSlide();
+}
+
+SlideSelector.prototype.setActiveSlide = function(){
+    var $module = this.module;
+    var $selectors = this.selectors;
+    var $slides = this.slides;
+
+    var breakpoint = this.breakpoint;
+
+    var slideCount = $slides.length;
+    var initialSlide = this.initialSlide;
+
+    if (initialSlide > slideCount) {
+        initialSlide = 1;
+    }
+
+    var $initialSelector = $selectors.filter(':nth-child('+initialSlide+')');
+    var $initialSlide = $slides.filter(':nth-child('+initialSlide+')');
+
+    $initialSelector.addClass('active');
+    $initialSlide.addClass('active');
+
+    function setActiveSlide(){
+        var mobileView = $(window).width() <= breakpoint;
+        var alternateMobileStyle = $module.hasClass('ss--mobile--default') == false;
+
+        $selectors.each(function(){
+            var $selector = $(this);
+            var i = $selector.index();
+            var $slide = $slides.eq(i);
+
+            var active = $selector.hasClass('active');
+
+            if (mobileView && alternateMobileStyle) {
+                $slide.css({'opacity': 1, 'display': 'block'});
+            } else {
+                if (active) {
+                    $slide.css({'opacity': 1, 'display': 'block'});
+                } else {
+                    $slide.css({'opacity': 1, 'display': 'none'});
+                }
+            }
+        });
+    }
+
+    setActiveSlide();
+    $(window).resize(setActiveSlide);
+};
+
+SlideSelector.prototype.setSlideHeight = function(){
+    var $module = this.module;
+    var $slides = this.slides;
+    var $slidesWrapper = this.slidesWrapper;
+    var breakpoint = this.breakpoint;
+
+    function setSlideHeight(){
+        var topHeight = 0;
+        var mobileView = $(window).width() <= breakpoint;
+        var defaultMobileStyle = $module.hasClass('ss--mobile--default');
+
+        $slides.each(function(){
+            var $slide = $(this);
+            var slideHeight = $slide.outerHeight();
+
+            if (slideHeight > topHeight) {
+                topHeight = slideHeight;
+            }
+        });
+
+        if (mobileView) {
+            if (defaultMobileStyle){
+                $slidesWrapper.css('height', topHeight);
+            } else {
+                $slidesWrapper.css('height', 'auto');
+            }
+        } else {
+            $slidesWrapper.css('height', topHeight);
+        }
+    }
+
+    setSlideHeight();
+    $(window).resize(setSlideHeight);
+};
+
+SlideSelector.prototype.changeActiveSlide = function(){
+    var $selectors = this.selectors;
+    var $slides = this.slides;
+    var speed = this.speed;
+
+    $selectors.on('click', function(){
+        var $selector = $(this);
+        var i = $selector.index();
+        var $slide = $slides.eq(i);
+
+        $selectors.removeClass('active');
+        $slides.removeClass('active').stop().animate({'opacity': 0}, speed, function(){
+            $slides.css('display', 'none');
+            $selector.addClass('active');
+            $slide.css('display', 'block').addClass('active').stop().animate({'opacity': 1}, speed);
+        });
+    });
+};
+
+SlideSelector.prototype.setMobileStyle = function(){
+    var object = this;
+    var $module = this.module;
+    var mobileStyle = this.mobileStyle;
+    var breakpoint = this.breakpoint;
+
+    object.buildMobileLayout();
+
+    switch (mobileStyle) {
+        case 'list':
+            $module.addClass('ss--mobile--list');
+            break;
+        case 'toggle':
+            $module.addClass('ss--mobile--toggle');
+            object.enableToggles();
+            break;
+        default:
+            $module.addClass('ss--mobile--default');
+            break;
+    }
+
+    function setMobileState(){
+        var mobileView = $(window).width() <= breakpoint;
+
+        if (mobileView) {
+            $module.addClass('ss--mobile');
+        } else {
+            $module.removeClass('ss--mobile');
+        }
+    }
+
+    setMobileState();
+    $(window).resize(setMobileState);
+};
+
+SlideSelector.prototype.buildMobileLayout = function(){
+    var $selectors = this.selectors;
+    var $slides = this.slides;
+
+    $selectors.each(function(){
+        var $selector = $(this);
+        var i = $selector.index();
+        var $slide = $slides.eq(i);
+        var selectorHtml = $selector.html();
+
+        $slide.wrapInner('<div class="ss__content__inner"></div>');
+        $slide.wrapInner('<div class="ss__content"></div>');
+        $slide.prepend('<div class="ss__label">'+selectorHtml+'</div>');
+    });
+};
+
+SlideSelector.prototype.enableToggles = function(){
+    var $slides = this.slides;
+    var $toggles = $slides.children('.ss__label');
+    var speed = this.speed;
+    var breakpoint = this.breakpoint;
+    var togglesOpen = this.togglesOpen;
+
+    if (togglesOpen) {
+        $slides.addClass('open');
+    }
+
+    function setContentHeight(){
+        var mobileView = $(window).width() <= breakpoint;
+
+        $slides.each(function(){
+            var $slide = $(this);
+            var $contentWrapper = $slide.children('.ss__content');
+            var $content = $contentWrapper.children('.ss__content__inner');
+            var open = $slide.hasClass('open');
+            var contentHeight = $content.outerHeight();
+
+            if (mobileView) {
+                if (open) {
+                    $contentWrapper.css('height', contentHeight);
+                    $content.css('opacity', 1);
+                } else {
+                    $contentWrapper.css('height', 0);
+                    $content.css('opacity', 0);
+                }
+            } else {
+                $contentWrapper.css('height', 'auto');
+                $content.css('opacity', 1);
+            }
+        });
+    }
+
+    setContentHeight();
+    $(window).resize(setContentHeight);
+
+    $toggles.on('click', function(){
+        var $toggle = $(this);
+        var $slide = $toggle.parent();
+        var $contentWrapper = $slide.children('.ss__content');
+        var $content = $contentWrapper.children('.ss__content__inner');
+        var contentHeight = $content.outerHeight();
+
+        $slide.toggleClass('open');
+
+        var open = $slide.hasClass('open');
+
+        if (open) {
+            $contentWrapper.stop().animate({'height': contentHeight}, speed, function(){
+                $content.stop().animate({'opacity': 1}, speed);
+            });
+        } else {
+            $content.stop().animate({'opacity': 0}, speed, function(){
+                $contentWrapper.stop().animate({'height': 0}, speed);
+            });
+        }
+    });
+};
+
+// use the below function to create your slideSelector.
+// if you have a customized version of the slideSelector with its own class/id, avoid using '.ss' and target that class/id instead.
+
+$('.ss').slideSelector();
+
+/*
+
+all settings below are set to their default values. use this as a reference.
+
+ $('.ss').slideSelector({
+     initialSlide: 1,
+     speed: 300,
+     breakpoint: 768,
+     // mobileStyle: 'toggle',
+     mobileOnly: false,
+     togglesOpen: false
+ });
+
+*/
